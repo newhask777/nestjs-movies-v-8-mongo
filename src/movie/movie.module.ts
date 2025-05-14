3146e7c495b5ce816956bc88788f6572150e36bc
newhask777 <newhaskellisp@gmail.com>
2025-05-14 13:03:44 +0300

    movies api

--- src/movie/movie.module.ts
+++ src/movie/movie.module.ts
@@ -1,9 +1,26 @@
-import { Module } from '@nestjs/common';
-import { MovieService } from './movie.service';
-import { MovieController } from './movie.controller';
+import { Module } from '@nestjs/common'
+import { TypegooseModule } from 'nestjs-typegoose'
+// import { TelegramModule } from 'src/telegram/telegram.module'
+import { UserModule } from 'src/user/user.module'
+import { MovieController } from './movie.controller'
+import { MovieModel } from './movie.model'
+import { MovieService } from './movie.service'
 
 @Module({
-  providers: [MovieService],
-  controllers: [MovieController]
+	controllers: [MovieController],
+	imports: [
+		TypegooseModule.forFeature([
+			{
+				typegooseClass: MovieModel,
+				schemaOptions: {
+					collection: 'Movie',
+				},
+			},
+		]),
+		// TelegramModule,
+		UserModule,
+	],
+	providers: [MovieService],
+	exports: [MovieService],
 })
 export class MovieModule {}
